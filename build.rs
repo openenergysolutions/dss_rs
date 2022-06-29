a8e406a35b2435348694a3889f48c246db0d8150
Timothy <tim@openenergysolutionsinc.com>
2022-06-29 12:25:46 -0700

    cargo fmt
    Signed-off-by: Timothy <tim@openenergysolutionsinc.com>

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() {
     let pwd = std::env::var("PWD").expect("No working directory?");
-    
-    let mut lib = pwd.clone(); 
+
+    let mut lib = pwd.clone();
     lib.push_str("/lib/linux_x64");
-    
+
     let mut link_search = String::from("cargo:rustc-link-search=-L");
     link_search.push_str(&lib);
     println!("{}", &link_search);
@@ -11,7 +11,7 @@ fn main() {
     let mut link_arg = String::from("cargo:rustc-link-arg=-L");
     link_arg.push_str(&lib);
     println!("{}", &link_arg);
-    
+
     let mut include = String::from("cargo:rustc-link-arg=-I");
     include.push_str(&pwd);
     include.push_str("/include");
