4ca73a3e48a3a79857f0d1e35b0e11776508b8cd
Timothy <tim@openenergysolutionsinc.com>
2022-06-30 10:35:47 -0700

    Fix namespacing
    Signed-off-by: Timothy <tim@openenergysolutionsinc.com>

--- tests/dss_start.rs
+++ tests/dss_start.rs
@@ -1,9 +1,9 @@
-use dss_rs::linux_x64::bindings as dss_rs;
+use dss_rs::linux_x64::bindings as dss_c;
 
 // Verify C API bindings can be called.
 #[test]
 fn test_dss_start() {
     unsafe {
-        dss_rs::DSS_Start(0);
+        dss_c::DSS_Start(0);
     }
 }
