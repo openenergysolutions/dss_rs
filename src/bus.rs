49fd380f99756836b0d407d4e250088fda26910c
Timothy <tim@openenergysolutionsinc.com>
2022-07-05 08:49:06 -0700

    Mark unsafe functions
    Signed-off-by: Timothy <tim@openenergysolutionsinc.com>

--- src/bus.rs
+++ src/bus.rs
@@ -1,11 +1,7 @@
-use crate::dss_result::{DssError, Result};
+use crate::dss_result::Result;
 #[cfg(feature = "linux_x64")]
 use crate::linux_x64::bindings as dss_c;
-use std::{
-    ffi::{CStr, CString},
-    os::raw::c_char,
-    ptr,
-};
+use std::{ffi::CStr, os::raw::c_char};
 
 pub fn get_name() -> Result<String> {
     unsafe {
@@ -19,8 +15,9 @@ pub fn get_num_nodes() -> i32 {
     unsafe { dss_c::Bus_Get_NumNodes() }
 }
 
-pub fn get_seq_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_seq_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_SeqVoltages(result_ptr, result_count);
 }
 
 pub fn get_seq_voltages_gr() {
@@ -29,8 +26,9 @@ pub fn get_seq_voltages_gr() {
     }
 }
 
-pub fn get_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_Voltages(result_ptr, result_count);
 }
 
 pub fn get_voltages_gr() {
@@ -39,8 +37,9 @@ pub fn get_voltages_gr() {
     }
 }
 
-pub fn get_nodes(result_ptr: *mut *mut i32, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_nodes(result_ptr: *mut *mut i32, result_count: *mut i32) {
+    dss_c::Bus_Get_Nodes(result_ptr, result_count);
 }
 
 pub fn get_nodes_gr() {
@@ -49,8 +48,9 @@ pub fn get_nodes_gr() {
     }
 }
 
-pub fn get_isc(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_isc(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_Isc(result_ptr, result_count);
 }
 
 pub fn get_isc_gr() {
@@ -59,8 +59,9 @@ pub fn get_isc_gr() {
     }
 }
 
-pub fn get_voc(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_voc(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_Voc(result_ptr, result_count);
 }
 
 pub fn get_voc_gr() {
@@ -73,8 +74,9 @@ pub fn get_kv_base() -> f64 {
     unsafe { dss_c::Bus_Get_kVBase() }
 }
 
-pub fn get_pu_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_pu_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_puVoltages(result_ptr, result_count);
 }
 
 pub fn get_pu_voltages_gr() {
@@ -83,8 +85,9 @@ pub fn get_pu_voltages_gr() {
     }
 }
 
-pub fn get_zsc0(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_zsc0(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_Zsc0(result_ptr, result_count);
 }
 
 pub fn get_zsc0_gr() {
@@ -93,8 +96,9 @@ pub fn get_zsc0_gr() {
     }
 }
 
-pub fn get_zsc1(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_zsc1(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_Zsc1(result_ptr, result_count);
 }
 
 pub fn get_zsc1_gr() {
@@ -103,8 +107,9 @@ pub fn get_zsc1_gr() {
     }
 }
 
-pub fn get_zsc_matrix(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_zsc_matrix(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_ZscMatrix(result_ptr, result_count);
 }
 
 pub fn get_zsc_matrix_gr() {
@@ -117,8 +122,9 @@ pub fn zscrefresh() -> u16 {
     unsafe { dss_c::Bus_ZscRefresh() }
 }
 
+#[cfg(feature = "unsafe")]
 pub fn get_ysc_matrix(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+    dss_c::Bus_Get_YscMatrix(result_ptr, result_count);
 }
 
 pub fn get_ysc_matrix_gr() {
@@ -159,8 +165,9 @@ pub fn get_unique_node_number(start_number: i32) -> i32 {
     unsafe { dss_c::Bus_GetUniqueNodeNumber(start_number) }
 }
 
-pub fn get_cplx_seq_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_cplx_seq_voltages(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_CplxSeqVoltages(result_ptr, result_count);
 }
 
 pub fn get_cplx_seq_voltages_gr() {
@@ -193,8 +200,9 @@ pub fn get_n_interrupts() -> f64 {
     unsafe { dss_c::Bus_Get_N_interrupts() }
 }
 
-pub fn get_pu_vll(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_pu_vll(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_puVLL(result_ptr, result_count);
 }
 
 pub fn get_pu_vll_gr() {
@@ -203,8 +211,9 @@ pub fn get_pu_vll_gr() {
     }
 }
 
-pub fn get_vll(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_vll(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_VLL(result_ptr, result_count);
 }
 
 pub fn get_vll_gr() {
@@ -213,8 +222,9 @@ pub fn get_vll_gr() {
     }
 }
 
-pub fn get_pu_vmag_angle(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_pu_vmag_angle(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_puVmagAngle(result_ptr, result_count);
 }
 
 pub fn get_pu_vmag_angle_gr() {
@@ -223,8 +233,9 @@ pub fn get_pu_vmag_angle_gr() {
     }
 }
 
-pub fn get_vmag_angle(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_vmag_angle(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_VMagAngle(result_ptr, result_count);
 }
 
 pub fn get_vmag_angle_gr() {
@@ -241,8 +252,12 @@ pub fn get_section_id() -> i32 {
     unsafe { dss_c::Bus_Get_SectionID() }
 }
 
-pub fn get_line_list(result_ptr: *mut *mut *mut ::std::os::raw::c_char, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_line_list(
+    result_ptr: *mut *mut *mut ::std::os::raw::c_char,
+    result_count: *mut i32,
+) {
+    dss_c::Bus_Get_LineList(result_ptr, result_count);
 }
 
 pub fn get_line_list_gr() {
@@ -251,8 +266,12 @@ pub fn get_line_list_gr() {
     }
 }
 
-pub fn get_load_list(result_ptr: *mut *mut *mut ::std::os::raw::c_char, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_load_list(
+    result_ptr: *mut *mut *mut ::std::os::raw::c_char,
+    result_count: *mut i32,
+) {
+    dss_c::Bus_Get_LoadList(result_ptr, result_count);
 }
 
 pub fn get_load_list_gr() {
@@ -261,8 +280,9 @@ pub fn get_load_list_gr() {
     }
 }
 
-pub fn get_zsc_012_matrix(result_ptr: *mut *mut f64, result_count: *mut i32) {
-    todo!()
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_zsc_012_matrix(result_ptr: *mut *mut f64, result_count: *mut i32) {
+    dss_c::Bus_Get_ZSC012Matrix(result_ptr, result_count);
 }
 
 pub fn get_zsc_012_matrix_gr() {
@@ -271,16 +291,18 @@ pub fn get_zsc_012_matrix_gr() {
     }
 }
 
-pub fn get_all_pce_at_bus(
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_all_pce_at_bus(
     result_ptr: *mut *mut *mut ::std::os::raw::c_char,
     result_count: *mut i32,
 ) {
-    todo!()
+    dss_c::Bus_Get_AllPCEatBus(result_ptr, result_count);
 }
 
-pub fn get_all_pde_at_bus(
+#[cfg(feature = "unsafe")]
+pub unsafe fn get_all_pde_at_bus(
     result_ptr: *mut *mut *mut ::std::os::raw::c_char,
     result_count: *mut i32,
 ) {
-    todo!()
+    dss_c::Bus_Get_AllPDEatBus(result_ptr, result_count);
 }
