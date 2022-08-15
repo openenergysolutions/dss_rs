a248df48cfa299ef137b5302af114763b9e3c4af
Timothy <tim@openenergysolutionsinc.com>
2022-08-15 15:44:43 -0700

    Get tests working
    Signed-off-by: Timothy <tim@openenergysolutionsinc.com>

---
+++ build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("cargo:rustc-env=LD_LIBRARY_PATH=dss_rs_sys/dss_capi/lib/linux_x64");
+}
--- tests/dss.rs
+++ tests/dss.rs
@@ -1,24 +1,7 @@
-extern crate dss_rs_sys;
-use dss_rs::{dss, circuit};
-
-// Verify C API bindings can be called.
-#[test]
-fn unsafe_dss_start() {
-    unsafe {
-        let ret = dss_rs_sys::DSS_Start(0);
-        assert!(ret != 0);
-    }
-}
+use dss_rs::dss;
 
 #[test]
 fn dss_start() {
     let ret = dss::start(0);
     assert!(ret != 0);
-}
-
-#[test]
-fn text_set_cmd() {
-    if let Err(_) = dss::text_set_command("Redirect ../../cvr_protoype/data/test_feeder/test_feeder.dss") {
-        panic!("FAILURE")
-    }
 }
