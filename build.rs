bb54fbe101056d57d344bf86c6789927ac6d853f
Timothy <tim@openenergysolutionsinc.com>
2022-08-16 15:05:29 -0700

    Specify rpath
    Signed-off-by: Timothy <tim@openenergysolutionsinc.com>

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("cargo:rustc-env=LD_LIBRARY_PATH=dss_rs_sys/dss_capi/lib/linux_x64");
+    let pwd = std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR not set");
+    let mut lib = pwd;
+    lib.push_str("/dss_rs_sys/dss_capi/lib/linux_x64");
+
+    println!("cargo:rustc-link-search=native={}", lib);
+    println!("cargo:rustc-link-arg=-Wl,-rpath={}", lib);
+    println!("cargo:rustc-link-lib=dss_capi");
 }
