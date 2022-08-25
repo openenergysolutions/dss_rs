25b5b0e26701509cee48d7bb6a7582132ef9a212
timothy <tim@openenergysolutionsinc.com>
2022-08-25 14:36:40 -0700

    Add Vsource methods
    Signed-off-by: timothy <tim@openenergysolutionsinc.com>

---
+++ src/vsources.rs
@@ -0,0 +1,19 @@
+extern crate dss_rs_sys;
+use crate::dss_result::Result;
+use dss_rs_sys as dss_c;
+use std::{
+    convert::TryInto,
+    ffi::CString,
+};
+
+pub fn set_name(value: &str) -> Result<()> {
+    unsafe {
+        let c_str = CString::new(value)?;
+        dss_c::Vsources_Set_Name(c_str.into_raw());
+    }
+    Ok(())
+}
+
+pub fn set_pu(value: f64) {
+    unsafe { dss_c::Vsources_Set_pu(value); }
+}
