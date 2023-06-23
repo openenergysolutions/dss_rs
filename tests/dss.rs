7c51c5b24e811c950122a35c8b3276411ff9d6ae
Tim Maloney <tim@openenergysolutionsinc.com>
2023-06-23 16:12:41 +0000

    OCP-2398: Refactor unit-test
    Signed-off-by: Tim Maloney <tim@openenergysolutionsinc.com>

--- tests/dss.rs
+++ tests/dss.rs
@@ -6,91 +6,29 @@ fn shared_lib_call() {
     assert!(ret != 0);
 }
 
-// #[test]
-// fn open_breaker_and_get_reading() {
-//     todo!();
-// }
-
-// #[test]
-// fn close_breaker_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn open_capbank_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn enable_load_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn disable_load_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn enable_generator_and_get_reading() {
-//     todo!()
-// }
-
 #[test]
-fn tap_regulator_up_and_get_reading() {
-    dss::start(0);
-    dss::text_set_command("redirect tests/data/IEEE13Nodeckt.dss");
+fn tap_regulator_and_get_reading() {
+    assert!(dss::start(0) != 0);
+    assert!(dss::text_set_command("redirect tests/data/IEEE13Nodeckt.dss").is_ok());
     assert!(circuit::set_active_element("regcontrol.Reg1").is_ok());
-
     let cur_tap = reg_controls::get_tap_number();
-    reg_controls::set_tap_number(cur_tap + 1);
+
+    // Raise Tap
+    let cur_tap = cur_tap + 1;
+    reg_controls::set_tap_number(cur_tap);
 
     assert!(circuit::set_active_element("Transformer.Reg1").is_ok());
 
-    let readings = ckt_element::get_voltages_mag_ang();
-    assert!(readings.is_ok());
+    let voltages = ckt_element::get_voltages_mag_ang();
+    let currents = ckt_element::get_powers();
+    assert!(voltages.is_ok());
+    assert!(currents.is_ok());
+
+    // Lower Tap
+    reg_controls::set_tap_number(cur_tap - 3);
+
+    let voltages = ckt_element::get_voltages_mag_ang();
+    let currents = ckt_element::get_powers();
+    assert!(voltages.is_ok());
+    assert!(currents.is_ok());
 }
-
-#[test]
-fn tap_regulator_down_and_get_reading() {
-    dss::start(0);
-    dss::text_set_command("redirect tests/data/IEEE13Nodeckt.dss");
-    assert!(circuit::set_active_element("regcontrol.Reg1").is_ok());
-
-    let cur_tap = reg_controls::get_tap_number();
-    reg_controls::set_tap_number(cur_tap - 1);
-
-    assert!(circuit::set_active_element("Transformer.Reg1").is_ok());
-    let readings = ckt_element::get_voltages_mag_ang();
-    assert!(readings.is_ok());
-}
-
-// #[test]
-// fn open_recloser_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn close_recloser_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn open_switch_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn close_switch_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn set_generator_and_get_reading() {
-//     todo!()
-// }
-
-// #[test]
-// fn set_load_and_get_reading() {
-//     todo!()
-// }
