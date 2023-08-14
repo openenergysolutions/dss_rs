7aba91366d470651922df6165572c45bffc35ea9
Justin Mager <justin@openenergysolutionsinc.com>
2023-08-14 14:43:43 -0700

    Add pv

---
+++ src/pv.rs
@@ -0,0 +1,40 @@
+extern crate dss_rs_sys;
+use crate::dss_result::Result;
+use dss_rs_sys as dss_c;
+use std::ffi::CString;
+
+
+pub fn set_name(value: &str) -> Result<()> {
+  unsafe {
+      let c_str = CString::new(value)?;
+      dss_c::PVSystems_Set_Name(c_str.into_raw());
+  }
+  Ok(())
+}
+
+
+pub fn set_kvar(value: f64) { 
+  unsafe {
+    dss_c::PVSystems_Set_kvar(value);
+  }
+}
+
+
+pub fn set_kw(value: f64) {
+  unsafe {
+      dss_c::PVSystems_Set_kW(value);
+  }
+}
+
+
+pub fn set_pf(value: f64) {
+    unsafe {
+        dss_c::PVSystems_Set_PF(value);
+    }
+}
+
+pub fn set_irradiance(value: f64) {
+    unsafe {
+      dss_c::PVSystems_Set_Irradiance(value);
+    }
+}
